a3064442a261806838e5d94df4f946de4c5b249f
aswly <aswly98@gmail.com>
2022-04-20 04:50:35 -0700

    add form validation

--- src/app/components/add-student/add-student.component.ts
+++ src/app/components/add-student/add-student.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Student } from 'src/app/models/student/student';
 import { StudentsService } from '../../students.service';
 @Component({
   selector: 'app-add-student',
@@ -8,17 +9,25 @@ import { StudentsService } from '../../students.service';
 })
 export class AddStudentComponent implements OnInit {
   message: boolean = false;
+  Errmessage: boolean = false;
 
   constructor(private studentsService: StudentsService) { }
   addStudent = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl('')
+    name: new FormControl('',Validators.required),
+    email: new FormControl('',[ Validators.required,Validators.email])
 
   });
   ngOnInit(): void {
   }
 
+  get email(){return this.addStudent.get('email')}
+  get name(){return this.addStudent.get('name')}
   SaveData() {
+    if(this.addStudent.invalid)
+    {
+      this.Errmessage=true;
+      return
+    }
     //console.log(this.addStudent.value)sdsdsdsd 
     this.studentsService.SaveStudentData(this.addStudent.value).subscribe((result) => {
 
@@ -30,6 +39,10 @@ export class AddStudentComponent implements OnInit {
   }
   removemessagee(){
     this.message = false;
+
+  }
+  removemErressagee(){
+    this.Errmessage = false;
 
   }
 }
